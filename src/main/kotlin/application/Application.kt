0c2b056adb66f2a31f6ffcb041dc553a5be8b01c
Mohamed Gara <mohamed.gara@external.grtgaz.com>
2021-06-21 23:32:29 +0200

    feat: A -> 5

---
+++ src/main/kotlin/application/Application.kt
@@ -0,0 +1,7 @@
+package application
+
+class Application {
+  fun transport(containers: String): Int {
+    return 5
+  }
+}
---
+++ src/test/kotlin/application/ApplicationTest.kt
@@ -0,0 +1,22 @@
+package application
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+
+internal class ApplicationTest {
+
+  private val sut = Application()
+
+  @TestFactory
+  fun `compute transport duration`() =
+    listOf(
+      "A" to 5
+    ).map { (containers, expectedDuration) ->
+      dynamicTest("$containers are transported in $expectedDuration hours") {
+        val result = sut.transport(containers)
+
+        assertThat(result).isEqualTo(expectedDuration)
+      }
+    }
+}
