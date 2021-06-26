c6a2a645b27750109d3a74113163367cdc219afc
Mohamed Gara <mohamed.gara@external.grtgaz.com>
2021-06-27 00:28:47 +0200

    refactor: introduce truck type

--- src/main/kotlin/application/Application.kt
+++ src/main/kotlin/application/Application.kt
@@ -4,13 +4,10 @@ class Application {
 
   var hour = 0
 
+  val truck1 = Truck("1")
+  val truck2 = Truck("2")
+
   var remainingContainer = ""
-
-  var truck1RemaningHoursToDeliver = 0
-  var truck2RemaningHoursToDeliver = 0
-
-  var truck1RemaningHoursToGoHome = 0
-  var truck2RemaningHoursToGoHome = 0
 
   fun transport(containers: String): Int {
     if (containers.isBlank()) return 0
@@ -18,40 +15,23 @@ class Application {
     remainingContainer = containers
 
     while (true) {
-      takeNextContainerByTruck1()
-      takeNextContainerByTruck2()
+      tryTakingNextContainerBy(truck1)
+      tryTakingNextContainerBy(truck2)
 
       hour++
 
-      moveTruck1()
-      moveTruck2()
+      truck1.move()
+      truck2.move()
 
-      if (remainingContainer.isEmpty() && truck1RemaningHoursToDeliver == 0 && truck2RemaningHoursToDeliver == 0) return hour
+      if (allContainersAreDelivred()) return hour
     }
   }
 
-  private fun moveTruck1() {
-    if (truck1RemaningHoursToDeliver > 0) truck1RemaningHoursToDeliver--
-    else if (truck1RemaningHoursToGoHome > 0) truck1RemaningHoursToGoHome--
+  private fun tryTakingNextContainerBy(truck: Truck) {
+    if (truck.tryTakeContainer())
+      remainingContainer = remainingContainer.drop(1)
   }
 
-  private fun moveTruck2() {
-    if (truck2RemaningHoursToDeliver > 0) truck2RemaningHoursToDeliver--
-    else if (truck2RemaningHoursToGoHome > 0) truck2RemaningHoursToGoHome--
-  }
-
-  private fun takeNextContainerByTruck1() {
-    if (truck1RemaningHoursToDeliver > 0 || truck1RemaningHoursToGoHome > 0) return
-    remainingContainer = remainingContainer.drop(1)
-    truck1RemaningHoursToDeliver = 5
-    truck1RemaningHoursToGoHome = 5
-  }
-
-  private fun takeNextContainerByTruck2() {
-    if (truck2RemaningHoursToDeliver > 0 || truck2RemaningHoursToGoHome > 0) return
-
-    remainingContainer = remainingContainer.drop(1)
-    truck2RemaningHoursToDeliver = 5
-    truck2RemaningHoursToGoHome = 5
-  }
+  private fun allContainersAreDelivred() =
+    remainingContainer.isEmpty() && truck1.remaningHoursToDeliver == 0 && truck2.remaningHoursToDeliver == 0
 }
---
+++ src/main/kotlin/application/Truck.kt
@@ -0,0 +1,26 @@
+package application
+
+data class Truck(
+  var id: String,
+  var remaningHoursToDeliver: Int = 0,
+  var remaningHoursToGoHome: Int = 0
+) {
+
+  fun isDriving() = remaningHoursToDeliver > 0 || remaningHoursToGoHome > 0
+
+  fun startDrive() {
+    remaningHoursToDeliver = 5
+    remaningHoursToGoHome = 5
+  }
+
+  fun move() {
+    if (remaningHoursToDeliver > 0) remaningHoursToDeliver--
+    else if (remaningHoursToGoHome > 0) remaningHoursToGoHome--
+  }
+
+  fun tryTakeContainer(): Boolean {
+    if (isDriving()) return false
+    startDrive()
+    return true
+  }
+}
