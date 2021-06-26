715ab16c44d7261d6546362a88545715b3e84f97
Mohamed Gara <mohamed.gara@external.grtgaz.com>
2021-06-27 01:36:26 +0200

    test: extract method to simplify test creation

--- src/test/kotlin/application/ApplicationTest.kt
+++ src/test/kotlin/application/ApplicationTest.kt
@@ -1,24 +1,27 @@
 package application
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
 
 internal class ApplicationTest {
 
   @TestFactory
-  fun `compute transport duration`() =
-    listOf(
-      "" to 0,
-      "B" to 5,
-      "BB" to 5,
-      "BBB" to 15,
-      "BBBB" to 15,
-      "BBBBB" to 25,
-      "BBBBBB" to 25,
-      "BBBBBBB" to 35,
-      "BBBBBBBB" to 35,
-    ).map { (containers, expectedDuration) ->
+  fun `compute transport duration for only B destination`(): List<DynamicTest> = tests(
+    "" to 0,
+    "B" to 5,
+    "BB" to 5,
+    "BBB" to 15,
+    "BBBB" to 15,
+    "BBBBB" to 25,
+    "BBBBBB" to 25,
+    "BBBBBBB" to 35,
+    "BBBBBBBB" to 35,
+  )
+
+  fun tests(vararg tests: Pair<String, Int>): List<DynamicTest> =
+    listOf(*tests).map { (containers, expectedDuration) ->
       dynamicTest("$containers are transported in $expectedDuration hours") {
         val sut = application.Application()
 
