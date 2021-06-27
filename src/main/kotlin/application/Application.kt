dfe656a1e7cea1dcc8be9bad7aa80843408395d2
Mohamed Gara <mohamed.gara@external.grtgaz.com>
2021-06-27 20:57:33 +0200

    refactor: move all state manipulation code to State class

--- src/main/kotlin/application/Application.kt
+++ src/main/kotlin/application/Application.kt
@@ -7,44 +7,9 @@ class Application {
   fun transport(containers: String): Int {
     return simulator.finishHourFor(
       initialState = State(remainingContainerForTrucks=containers) ,
-      eachHour = ::calculateNextState,
-      stopWhen = ::allContainersAreDelivered
+      eachHour = State::calculateNextState,
+      stopWhen = State::allContainersAreDelivered
     )
   }
-
-  private fun calculateNextState(state: State): State {
-    tryTakingNextContainerByTruck(state, state.truck1)
-    tryTakingNextContainerByTruck(state, state.truck2)
-    tryTakingNextContainerByShip(state, state.ship)
-
-    state.containerHandlers.forEach { it.move() }
-
-    state.trucks.forEach {
-      if (it.isAtPort()) state.remainingContainerForShip++
-    }
-
-    return state
-  }
-
-  private fun tryTakingNextContainerByTruck(state: State, truck: ContainerHandler) {
-    if (state.remainingContainerForTrucks.isBlank()) return
-    if (truck.tryTakeContainer(state.remainingContainerForTrucks.first())) {
-      state.remainingContainerForTrucks = state.remainingContainerForTrucks.drop(1)
-    }
-  }
-
-  private fun tryTakingNextContainerByShip(state: State, ship: ContainerHandler) {
-    if (state.remainingContainerForShip == 0) return
-    if (ship.tryTakeContainer('A')) {
-      state.remainingContainerForShip--
-    }
-  }
-
-  private fun allContainersAreDelivered(state: State): Boolean =
-    state.remainingContainerForTrucks.isEmpty()
-        && state.remainingContainerForShip == 0
-        && state.truck1.hasNoDeliveryInProgress()
-        && state.truck2.hasNoDeliveryInProgress()
-        && state.ship.hasNoDeliveryInProgress()
 }
 
--- src/main/kotlin/application/State.kt
+++ src/main/kotlin/application/State.kt
@@ -13,4 +13,37 @@ data class State(
 
   val trucks: List<Truck>
     get() = listOf(truck1, truck2)
+
+  fun allContainersAreDelivered(): Boolean =
+    remainingContainerForTrucks.isEmpty()
+        && remainingContainerForShip == 0
+        && containerHandlers.all { it.hasNoDeliveryInProgress() }
+
+  fun calculateNextState(): State {
+    tryTakingNextContainerByTruck(truck1)
+    tryTakingNextContainerByTruck(truck2)
+    tryTakingNextContainerByShip(ship)
+
+    containerHandlers.forEach { it.move() }
+
+    trucks.forEach {
+      if (it.isAtPort()) remainingContainerForShip++
+    }
+
+    return this
+  }
+
+  private fun tryTakingNextContainerByTruck(truck: ContainerHandler) {
+    if (remainingContainerForTrucks.isBlank()) return
+    if (truck.tryTakeContainer(remainingContainerForTrucks.first())) {
+      remainingContainerForTrucks = remainingContainerForTrucks.drop(1)
+    }
+  }
+
+  private fun tryTakingNextContainerByShip(ship: ContainerHandler) {
+    if (remainingContainerForShip == 0) return
+    if (ship.tryTakeContainer('A')) {
+      remainingContainerForShip--
+    }
+  }
 }
