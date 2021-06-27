852c9959aa54efd225cdf04a2019be366fdd34df
Mohamed Gara <mohamed.gara@external.grtgaz.com>
2021-06-28 01:01:05 +0200

    feat: add log messages

--- src/main/kotlin/application/TimeSimulator.kt
+++ src/main/kotlin/application/TimeSimulator.kt
@@ -1,7 +1,10 @@
 package application
 
+import java.lang.RuntimeException
+
 data class TimeSimulator(
   private var hour: Int = 0,
+  private val logEnabled: Boolean = false
 ) {
 
   fun finishHourFor(
@@ -13,15 +16,30 @@ data class TimeSimulator(
     var currentState = initialState
 
     while (true) {
-      if (stopWhen(currentState)) return hour
+      logMessage("Tick: hour $hour")
+
+      if (stopWhen(currentState)) {
+        logMessage("Stop at hour: $hour")
+        return hour
+      }
+
+      logMessage("state at $hour (begin): $currentState")
 
       currentState = eachHour(currentState)
 
+      logMessage("state at $hour (end)  : $currentState")
+
       advanceToNextHour()
+
+      if (hour == 100) throw RuntimeException("Too much iterations...")
     }
   }
 
   private fun advanceToNextHour() {
     hour++
+  }
+
+  private fun logMessage(msg: String) {
+    if (logEnabled) println(msg)
   }
 }
