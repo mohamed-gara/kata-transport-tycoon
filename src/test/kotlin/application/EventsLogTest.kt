953cdd804cdff5b134a54f800733180b94df7a18
Mohamed Gara <mohamed.gara@external.grtgaz.com>
2021-07-25 12:47:39 +0200

    test: extract expectedEventsFrom method

--- src/test/kotlin/application/EventsLogTest.kt
+++ src/test/kotlin/application/EventsLogTest.kt
@@ -1,6 +1,5 @@
 package application
 
-import application.carriers.Cargo
 import application.carriers.TransportEvent
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -14,70 +13,12 @@ internal class EventsLogTest {
 
     app.transport("AB")
 
-    val fileContent = javaClass.classLoader.getResourceAsStream("AB_expected_events.json")
-    val expected_events_for_AB: List<TransportEvent> = jacksonObjectMapper().readValue(fileContent)
+    assertThat(app.state.events)
+      .containsExactlyElementsOf(expectedEventsFrom("AB_expected_events.json"))
+  }
 
-    assertThat(app.state.events).containsExactlyElementsOf(expected_events_for_AB)
-    assertThat(expected_events_for_AB)
-      .containsExactly(
-        TransportEvent(
-          id = "",
-          event = "DEPART",
-          time = 0,
-          transport_id = 0,
-          kind = "TRUCK",
-          location = "FACTORY",
-          destination = "PORT",
-          cargo = listOf(
-            Cargo(
-              cargo_id = "0",
-              destination = "A",
-              origin = "FACTORY",
-            )
-          ),
-        ),
-        TransportEvent(
-          id = "",
-          event = "DEPART",
-          time = 0,
-          transport_id = 1,
-          kind = "TRUCK",
-          location = "FACTORY",
-          destination = "B",
-          cargo = listOf(
-            Cargo(
-              cargo_id = "1",
-              destination = "B",
-              origin = "FACTORY",
-            )
-          ),
-        ),
-        TransportEvent(
-          id = "",
-          event = "ARRIVE",
-          time = 1,
-          transport_id = 0,
-          kind = "TRUCK",
-          location = "PORT",
-          destination = "",
-          cargo = listOf(
-            Cargo(
-              cargo_id = "0",
-              destination = "A",
-              origin = "FACTORY"
-            )
-          )
-        ),
-        TransportEvent(
-          id = "",
-          event = "DEPART",
-          time = 1,
-          transport_id = 0,
-          kind = "TRUCK",
-          location = "PORT",
-          destination = "FACTORY",
-          cargo = listOf()
-        ),
-      )
+  fun expectedEventsFrom(file: String): List<TransportEvent> {
+    val fileContent = javaClass.classLoader.getResourceAsStream(file)
+    return jacksonObjectMapper().readValue(fileContent)
   }
 }
